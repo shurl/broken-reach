cfe28298dcd2eb7eaa52642304fbb3869c976776
shurl <15345515+shurl@users.noreply.github.com>
2024-08-30 19:08:40 +0100

    Update data (cron)

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 cp /ping.log $(dirname "$0")/ping.log
-gzip $(dirname "$0")/ping.log
+gzip -f $(dirname "$0")/ping.log
 rm  $(dirname "$0")/ping.log
 git commit -a -m "Update data (cron)"
 git push
